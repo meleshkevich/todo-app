cbc6955e8b58777a90f908a740b02039d922f6ae
Ilja M <i.meleshkevich@gmail.com>
2022-07-04 23:41:56 +0200

    build created

---
+++ build/precache-manifest.08b033740a19ffc2b3a9a1a1dbdb40a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76794e1ab021519610bf4dee041e5d16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3febf68aee8c0a045a",
+    "url": "/static/css/main.f902a80e.chunk.css"
+  },
+  {
+    "revision": "eb2e3e7f154677808351",
+    "url": "/static/js/2.44a4508b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44a4508b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3febf68aee8c0a045a",
+    "url": "/static/js/main.9d8e86c5.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
